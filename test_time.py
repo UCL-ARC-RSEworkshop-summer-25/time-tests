05b4fc4dd02848619ad693bbcd215a876d5d492f
XinweiJu <xinwei.ju.22@ucl.ac.uk>
2025-06-25 17:08:10 +0100

    Add pytest.mark.parametrize (with multiple inputs and IDs) and did ruff check; Answers UCL-ARC-RSEworkshop-summer-25/RSE-Classwork#17

--- test_time.py
+++ test_time.py
@@ -1,30 +1,39 @@
 from times import time_range, compute_overlap_time
 from pytest import raises
 
+
 def test_given_input():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
     result = compute_overlap_time(large, short)
-    expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
-    
+    expected = [
+        ("2010-01-12 10:30:00", "2010-01-12 10:37:00"),
+        ("2010-01-12 10:38:00", "2010-01-12 10:45:00"),
+    ]
+
     assert result == expected
+
 
 def test_no_overlap():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-13 10:30:00", "2010-01-13 10:45:00", 2, 60)
     result = compute_overlap_time(large, short)
     expected = []
-    
+
     assert result == expected
+
 
 def test_multi_intervals():
     large = time_range("2025-01-01 00:00:00", "2025-01-01 00:11:00", 2, 60)
     short = time_range("2025-01-01 00:02:00", "2025-01-01 00:12:00", 4, 120)
     result = compute_overlap_time(large, short)
     print(result)
-    expected = [('2025-01-01 00:02:00', '2025-01-01 00:03:00'),
-                ('2025-01-01 00:08:00', '2025-01-01 00:09:00')] 
+    expected = [
+        ("2025-01-01 00:02:00", "2025-01-01 00:03:00"),
+        ("2025-01-01 00:08:00", "2025-01-01 00:09:00"),
+    ]
     assert result == expected
+
 
 def test_edge_case():
     first = time_range("2025-06-25 00:00:00", "2025-06-25 00:11:00")
@@ -33,6 +42,7 @@ def test_edge_case():
     expected = []
     assert result == expected
 
+
 def test_start_ends():
-    with raises(ValueError, match = r"Start time must be before end time"):
+    with raises(ValueError, match=r"Start time must be before end time"):
         time_range("2025-01-01 00:01:00", "2025-01-01 00:00:00")
--- times.py
+++ times.py
@@ -6,11 +6,24 @@ def time_range(start_time, end_time, number_of_intervals=1, gap_between_interval
     end_time_s = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
     if start_time_s > end_time_s:
         raise ValueError("Start time must be before end time")
-    d = (end_time_s - start_time_s).total_seconds() / number_of_intervals + gap_between_intervals_s * (1 / number_of_intervals - 1)
-    sec_range = [(start_time_s + datetime.timedelta(seconds=i * d + i * gap_between_intervals_s),
-                  start_time_s + datetime.timedelta(seconds=(i + 1) * d + i * gap_between_intervals_s))
-                 for i in range(number_of_intervals)]
-    return [(ta.strftime("%Y-%m-%d %H:%M:%S"), tb.strftime("%Y-%m-%d %H:%M:%S")) for ta, tb in sec_range]
+    d = (
+        end_time_s - start_time_s
+    ).total_seconds() / number_of_intervals + gap_between_intervals_s * (
+        1 / number_of_intervals - 1
+    )
+    sec_range = [
+        (
+            start_time_s
+            + datetime.timedelta(seconds=i * d + i * gap_between_intervals_s),
+            start_time_s
+            + datetime.timedelta(seconds=(i + 1) * d + i * gap_between_intervals_s),
+        )
+        for i in range(number_of_intervals)
+    ]
+    return [
+        (ta.strftime("%Y-%m-%d %H:%M:%S"), tb.strftime("%Y-%m-%d %H:%M:%S"))
+        for ta, tb in sec_range
+    ]
 
 
 def compute_overlap_time(range1, range2):
